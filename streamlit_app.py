5e463218fba53663ecc067f68d966deccebddb9a
mathasnowflake <131542815+mathasnowflake@users.noreply.github.com>
2023-04-22 13:02:10 -0500

    Update streamlit_app.py
    updated breakfast menu header and 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,7 @@ streamlit.text('Sri Rama')
 streamlit.text(' Sri Rama')
 
 streamlit.text(' Sri Rama')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
