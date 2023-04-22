428566ddd1005d8a3e8713aa0bf8e0badad85953
mathasnowflake <131542815+mathasnowflake@users.noreply.github.com>
2023-04-22 12:59:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,12 @@
 import streamlit
 
 streamlit.title('Jai Sri Rama')
+
+
+streamlit.header('Jai Sri Rama Nama')
+
+streamlit.text('Sri Rama')
+
+streamlit.text(' Sri Rama')
+
+streamlit.text(' Sri Rama')
