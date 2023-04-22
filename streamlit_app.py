46644c6f7135008f23f3721aef8d05ac49e26f78
mathasnowflake <131542815+mathasnowflake@users.noreply.github.com>
2023-04-22 13:18:16 -0500

    Update streamlit_app.py
    dataframe to access s3 file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('Jai Sri Rama')
 
@@ -16,4 +17,6 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Auocade Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
 
