72b57984b63d5779e82fc6047a037b38e12f22ab
mathasnowflake <131542815+mathasnowflake@users.noreply.github.com>
2023-04-24 20:26:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ import requests
 
 streamlit.header('Fruityvice Fruit Advice!')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-
+streamlit.text(fruityvice_response.json())
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #Streamline.text(fruityvice_response.json())
