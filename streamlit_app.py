4cd4a603d973b89e9e396ef4222ad685a7a65aa8
mathasnowflake <131542815+mathasnowflake@users.noreply.github.com>
2023-04-26 21:42:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,3 +41,5 @@ fruit_selected = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.in
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
+
+import snowflake.connector
