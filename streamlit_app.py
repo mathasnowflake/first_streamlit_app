09cfceaad6f4df4eb81db3662bcf527047b0d43f
mathasnowflake <131542815+mathasnowflake@users.noreply.github.com>
2023-04-28 11:01:04 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,7 @@ except URLError as e:
       streamlit.error()
 #fruit_selected = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
 #fruits_to_show = my_fruit_list.loc[fruits_selected]
-streamlit.stop()
+#streamlit.stop()
 #streamlit.dataframe(fruits_to_show)
 
 #Snowflake-related functions
