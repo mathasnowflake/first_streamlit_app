d3d25c8ec1d5a6d3ff0062236040dc238e2f93a7
mathasnowflake <131542815+mathasnowflake@users.noreply.github.com>
2023-04-27 22:42:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 #fruit_selected = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
 #fruits_to_show = my_fruit_list.loc[fruits_selected]
-
+streamlit.stop()
 #streamlit.dataframe(fruits_to_show)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
