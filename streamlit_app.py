9dcb277578bcc7cd57f7636c1eb5c76bf4450aef
mathasnowflake <131542815+mathasnowflake@users.noreply.github.com>
2023-04-27 23:30:20 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Create the repeatable code block (called a function)
-def get_fruityvice_data(this_fruit_choice)
+def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
     #streamlit.text(fruityvice_response.json())
     # write your own comment -what does the next line do? 
